2352491f9ff8efc09fda21cc641cdd26a238c188
bozzelliandrea <bozzelliandrea@yahoo.it>
2023-01-08 18:27:31 +0100

    react webapp setup

---
+++ illustrated-ml/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './Footer';
+import Home from './Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ illustrated-ml/src/Footer.jsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+    return (
+        <p>Copyright &copy;2023</p>
+    );
+}
