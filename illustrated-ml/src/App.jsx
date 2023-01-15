d8ddecd61abc008ed5b451af5fa4c1b223af75f4
bozzelliandrea <bozzelliandrea@yahoo.it>
2023-01-15 10:28:17 +0100

    navbar implementation

--- illustrated-ml/src/App.jsx
+++ illustrated-ml/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 import Footer from './Footer';
 import Home from './Home';
+import Navbar from './Navbar';
+
+let showNav = false;
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Navbar />
       <Home />
       <Footer />
     </div>
